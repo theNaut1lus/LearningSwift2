fde5116995a2be7e1c63c3a40834984c54726d78
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-26 18:19:44 +1100

    Initial Commit

---
+++ LearningSwift2/LearningSwift2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwift2App.swift
+//  LearningSwift2
+//
+//  Created by Sidak Singh Aulakh on 26/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwift2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
