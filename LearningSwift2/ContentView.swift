482e164e3c032e98ad0627750485c52da2fc7b78
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-04-01 20:16:42 +1100

    Class 26/03

--- LearningSwift2/ContentView.swift
+++ LearningSwift2/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var step = 22
+    @State var color: Color = .gray
+    @State var date: Date = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(color)
+                .ignoresSafeArea()
+            VStack {
+                Text("Stepper, ColorPicker and DatePicker View")
+                    .foregroundStyle(.regularMaterial)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                //for each range thingy example
+                ForEach(0...5, id:\.self) {
+                    i in Text(String(i))
+                }
+                Stepper(value: $step, in: 0...5) {
+                    Text(String(step))
+                }
+                ColorPicker("Color Picker", selection: $color)
+                DatePicker("Pick a Date", selection: $date, displayedComponents: .date)
+                Text(DateFormatter.localizedString(from: date, dateStyle: .full, timeStyle: .short))
+                Text(DateFormatter.month.string(from: date))
+            }
         }
-        .padding()
+    }
+}
+extension DateFormatter {
+    static var month: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM"
+        return formatter
     }
 }
 
